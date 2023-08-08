62ca1cb74883b03f564682dd4c3e58ff1a0cc11f
stephen-sanchez1 <sanchezstephen002@gmail.com>
2023-08-08 23:26:49 +0000

    added new main and module files

---
+++ main.py
@@ -0,0 +1,7 @@
+import module1 
+
+output1 = module1.additon(5, 10)
+output2 = module1.subtraction(5, 10)
+
+print ('Output: ',output1)
+print ('Output: ',output2)
---
+++ module1.py
@@ -0,0 +1,8 @@
+def additon(var1, var2) : 
+    output = var1 + var2 
+    return output 
+
+def subtraction(var1, var2) : 
+    output = var1 - var2 
+    return output 
+
